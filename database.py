3b66db4b25b03bb8a2cbef167aa737592a8ed607
Minwook11 <alsdnr4874@gmail.com>
2024-03-28 16:24:41 +0900

    [Add] SQLAlchemy를 통한 DB Migration 적용  * https://wikidocs.net/175967   - 위 링크 따라 진행  - Credential 데이터는 Git을 통한 관리 대상에서 제외하는 게 일반적   - 개인 공부 목적이므로 관리  * 문제점   - 번거로운 설정 작업이 확인됨    - alembic.ini 및 env.py 코드 수정 사항들   - 최초 설정 작업이 직관적이지 못한 듯 하다.    * Django, Flask-Migrate와 비교했을 때 기준.

---
+++ database.py
@@ -0,0 +1,17 @@
+import json
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+with open("db_credential.json", "r") as db_info_file:
+    db_data = json.load(db_info_file)
+    
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_data['USER']}:{db_data['PASSWORD']}@{db_data['HOST']}:{db_data['PORT']}/{db_data['DB_NAME']}"
+print(SQLALCHEMY_DATABASE_URL)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
