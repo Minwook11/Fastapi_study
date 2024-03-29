ad6c843d0dd90c7bc26ba2b693b018017db8cb24
Minwook11 <alsdnr4874@gmail.com>
2024-03-29 13:17:24 +0900

    [Add] Question Detail API 추가  - Path Parameter를 사용하여 Question 대상을 특정

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -10,3 +10,12 @@ def get_question_list(db: Session):
     ).all()
     
     return question_list
+
+def get_question(db: Session, question_id: int):
+    question = db.query(
+        Question
+    ).get(
+        question_id
+    )
+    
+    return question
