a889a888aca745dd437e13d2e74818d1dfd386a2
Minwook11 <alsdnr4874@gmail.com>
2024-03-29 10:23:01 +0900

    [Add] Pydantic 기반의 Schema 적용  - 이 단계에서는 API의 Response 형태 Schama를 정의

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -3,13 +3,14 @@ from sqlalchemy.orm import Session
 
 from database import get_db
 from models import Question
+from domain.question import question_schema
 
 
 router = APIRouter(
     prefix="/api/question",
 )
 
-@router.get("/list")
+@router.get("/list", response_model=list[question_schema.Question])
 async def question_list(db: Session = Depends(get_db)):
     _question_list = db.query(
         Question
